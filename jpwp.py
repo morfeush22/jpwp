04b73d3af7dde875a82e26cb45d24361290faa54
morfeush22 <patro.kacper@gmail.com>
2015-01-19 22:04:36 +0100

    DB works

--- jpwp.py
+++ jpwp.py
@@ -8,85 +8,101 @@ from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
 class ParseQuery(object):
-	def __init__(self):
+	def __init__(self, db):
 		self.countryQueryRe = re.compile(r"^country\(([a-zA-Z]*)\)$")
 		self.countryTagQueryRe = re.compile(r"^country\(([a-zA-Z]*)\);?tag\(([0-9a-zA-Z_.,:]*)\)$")
 		self.countryGetFlagQueryRe = re.compile(r"^country\(([a-zA-Z]*)\);?getflag$")
 		self.countryCheckFlagRe = re.compile(r"^checkflag\((http://[0-9a-zA-Z_.]*[gif|jpeg])\)$")
 		self.countryJson = re.compile(r"^([0-9a-zA-Z_]*)={([0-9a-zA-Z_,:]+)}$")
 
+		self.db = db
+
 	def __call__(self, query):
-		wrapper = Wrapper()
+		wrapper = MatchWrapper()
 		if wrapper(self.countryQueryRe.match(query)):
 			# get country info from wiki
 			country = wrapper.match.group(1)
-			getCountry(country)	
+			print self.__getCountry(country)	
 		elif wrapper(self.countryTagQueryRe.match(query)):
 			# get country info from wiki and get sentences with tags
 			country = wrapper.match.group(1)
 			tag = wrapper.match.group(2)
-			getCountryTag(country, tag)
+			print self.__getCountryTag(country, tag)
 		elif wrapper(self.countryGetFlagQueryRe.match(query)):
 			# return flag
 			country = wrapper.match.group(1)
-			getCountryFlag(country)
+			self.__getCountryFlag(country)
 		elif wrapper(self.countryCheckFlagRe.match(query)):
 			# check flag
 			url = wrapper.match.group(1)
-			checkCountryFlag(url)
+			self.__checkCountryFlag(url)
 		elif wrapper(self.countryJson.match(query)):
 			# json query
 			variable = wrapper.match.group(1)
 			query = wrapper.match.group(2)
-			handleJsonQuery(variable, query)
+			self.__handleJsonQuery(variable, query)
 		else:
 			# how to query
 			print "???"
 
-class Wrapper(object):
+	def __getCountry(self, country):
+		contents = self.db.contents
+		names = self.db.names
+
+		getItem = names.find({"name": country})
+		if getItem.count():
+			return contents.find({"name_id": getItem[0]["_id"]})[0]["content"]
+		else:
+			url = "http://en.wikipedia.org/wiki/" + country
+			rawHtml = urllib2.urlopen(url)
+			soup = BeautifulSoup(rawHtml)
+
+			divId = {"id": re.compile("mw-content-text")}
+			content = soup.find(attrs = divId)
+			outputText = re.sub("\s+", " ", content.text).strip()
+
+			postName = {"name": country}
+			postId = names.insert(postName)
+			
+			postContent = {"name_id": postId, "content": outputText}
+			contents.insert(postContent)
+
+			return outputText
+
+	def __getCountryTag(self, country, tag):
+		outputText = getCountry(country)
+		# + tag
+		pass
+
+	def __getCountryFlag(self, country):
+		pass
+
+	def __checkCountryFlag(self, imageUrl):
+		pass
+
+	def __handleJsonQuery(self, variable, query):
+		pass
+
+class MatchWrapper(object):
 	def __init__(self):
 		self.match = None
 
-	def __call__(self, regex):
-		if regex:
-			self.match = regex
+	def __call__(self, item):
+		if item:
+			self.match = item
 			return True
 		else:
 			return False
-
-def getCountry(country):
-	url = "http://en.wikipedia.org/wiki/" + country
-	rawHtml = urllib2.urlopen(url)
-	soup = BeautifulSoup(rawHtml)
-
-	divId = {"id": re.compile("mw-content-text")}
-	content = soup.find(attrs = divId)
-	outputText = re.sub("\s+", " ", content.text).strip()
-
-	return outputText
-
-def getCountryTag(country, tag):
-	getCountry(country)
-	# + tag
-	pass
-
-def getCountryFlag(country):
-	pass
-
-def checkCountryFlag(imageUrl):
-	pass
-
-def handleJsonQuery(variable, query):
-	pass
 
 if __name__ == "__main__":
 
 	client = MongoClient("mongodb://admin:admin@ds056727.mongolab.com:56727/countries")
 	db = client.countries
-	queryParser = ParseQuery()
+	queryParser = ParseQuery(db)
 
 	while True:
-		queryParser(re.sub(r"\s+", "", raw_input()))
+		print "Go on"
+		queryParser(re.sub(r"\s+", "", raw_input().lower()))
 
 	print client
 	print db
