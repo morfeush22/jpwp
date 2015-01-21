c151765464da2a5566ac0c63f87ea23bcbe3df7b
morfeush22 <patro.kacper@gmail.com>
2015-01-21 02:12:32 +0100

    Server

--- jpwp.py
+++ jpwp.py
@@ -3,9 +3,13 @@
 # ylnvsspc1
 from parsers import CountryParser, CountryTagParser, CountryJsonParser, CountryGetFlagParser, CountryCheckFlagParser
 from pymongo import MongoClient
+from threading import Thread
 
 import re
 import sys
+import tornado.escape
+import tornado.ioloop
+import tornado.web
 
 class ParseQuery(object):
 	def __init__(self, db):
@@ -32,10 +36,23 @@ class ParseQuery(object):
 
 		raise ValueError("Wrong query!")
 
+class RequestHandler(tornado.web.RequestHandler):
+	def post(self):
+		data = tornado.escape.json_decode(self.request.body)
+		self.write(data)
+
 if __name__ == "__main__":
 	_client = MongoClient("mongodb://admin:admin@ds056727.mongolab.com:56727/countries")
 	_db = _client.countries
 	_queryParser = ParseQuery(_db)
+
+	if len(sys.argv) == 2 and sys.argv[1] == "sm":
+		print "Server mode!"
+		server = tornado.web.Application([(r"/", RequestHandler), ])
+		server.listen(8888)
+		serverThread = Thread(target=tornado.ioloop.IOLoop.instance().start)
+		serverThread.daemon = True
+		serverThread.start()
 
 	while True:
 		print _queryParser(re.sub(r"\s+", "", raw_input("--> ").lower()))
